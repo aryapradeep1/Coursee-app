f19b4d5c505aff2bf27ea68bd643201efdef087d
aryapradeep1 <aaryapradeep04@gmail.com>
2026-07-11 15:43:16 +0530

    Add-completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddCourse from './components/AddCourse'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <AddCourse/>
     </>
   )
 }
